3753c9a079b8ce603fb14211459d77bebf50734b
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-08-29 23:05:29 +0600

    home text contents done

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,7 +5,7 @@ export default function Main() {
                 <div className="w-full">
                     <img src="/public/hero-mobile.jpg" alt="hero image" className="w-full h-full object-cover" />
                 </div>
-                {/* <div className="w-full min-h-50 py-19 flex items-center justify-center flex-col">
+                <div className="w-full min-h-50 py-19 flex items-center justify-center flex-col">
                     <div className="w-full text-center flex items-center flex-col">
                         <div className="w-full text-center">
                             <h1 className="text-5xl font-light text-primary-pink-400 uppercase tracking-[0.8rem]">we're</h1>
@@ -17,7 +17,7 @@ export default function Main() {
                     <div className="w-full max-w-2xl text-center px-5 mt-5">
                         <p className="text-base font-normal text-primary-pink-400">Hello fellow shoppers! We're currently building our new fashion store. Add you email below to stay up-to-date with announcements add our launch deals.</p>
                     </div>
-                </div> */}
+                </div>
             </main>
         </>
     );
