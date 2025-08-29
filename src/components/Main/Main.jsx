e787e03aed92c3c0d2ab643c571889ba08181c00
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-08-29 23:04:59 +0600

    Main.jsx image import and place the image in the top after the header

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,24 @@
 export default function Main() {
     return (
         <>
-            
+            <main>
+                <div className="w-full">
+                    <img src="/public/hero-mobile.jpg" alt="hero image" className="w-full h-full object-cover" />
+                </div>
+                {/* <div className="w-full min-h-50 py-19 flex items-center justify-center flex-col">
+                    <div className="w-full text-center flex items-center flex-col">
+                        <div className="w-full text-center">
+                            <h1 className="text-5xl font-light text-primary-pink-400 uppercase tracking-[0.8rem]">we're</h1>
+                        </div>
+                        <div className="w-full max-w-2xl">
+                            <h1 className="text-5xl font-semibold text-neutral-gray-900 uppercase tracking-[0.8rem] leading-14">Coming<br />Soon</h1>
+                        </div>
+                    </div>
+                    <div className="w-full max-w-2xl text-center px-5 mt-5">
+                        <p className="text-base font-normal text-primary-pink-400">Hello fellow shoppers! We're currently building our new fashion store. Add you email below to stay up-to-date with announcements add our launch deals.</p>
+                    </div>
+                </div> */}
+            </main>
         </>
     );
 };
