305889b4df88f27e00d9c293aeefcd34326d7add
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-08-29 21:59:10 +0600

    Header.jsx import logo and logo design done with mobile screen

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,11 @@
 export default function Header() {
     return (
         <>
-            
+            <header className="p-8">
+                <div className="w-auto">
+                    <img src="/public/logo.svg" alt="logo" className="w-32" />
+                </div>
+            </header>
         </>
     );
 };
